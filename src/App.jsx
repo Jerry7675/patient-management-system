198102b0c6b7b2eb1fd8305587c10f57bc9e88db
Jerry 7675 <sandeshchhetri5677@gmail.com>
2025-05-28 18:04:57 +0545

    fixed version compability issues among dependencies and tested the patient dashboard access logic(passed)

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import PatientDashboard from './pages/patient/PatientDashboard.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (<div>hello
-    
-  </div>
-    
+  return (
+   <div className="flex flex-col justify-center items-center h-screen text-3xl text-red-500 bg-gradient-to-r from-red-100 to-red-500">
+  testing
+</div>
+
   )
 }
 
--- src/pages/patient/PatientDashboard.jsx
+++ src/pages/patient/PatientDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../contexts/AuthContext';
-import { db } from '../firebase/config';
+import { useAuth } from '../../contexts/AuthContext.jsx';
+import { db } from '../../services/firebase/config';
 import { 
   collection, 
   query, 
